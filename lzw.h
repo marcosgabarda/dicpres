bd3abbf7edca78a0786ef1c06d9ab6cc68ec66d5
cyberslas <cyberslas@c9134505-e34d-0410-8303-e1b79613182d>
2008-05-30 10:30:50 +0000

    Idem.

---
+++ lzw.h
@@ -0,0 +1,43 @@
+#ifndef _LZW_H
+#define _LZW_H
+
+#include <string>
+#include <vector>
+#include "types.h"
+
+using namespace std;
+
+/**
+ * Clase que implementa la compresion de ficheros por el metodo de dicionario
+ * LZW.
+ * \author Iván Rodriguez Sastre & Marcos Gabarda Inat
+ */
+class lzw {
+ private:
+
+  /**
+   * Buffer que contiene todo el fichero leido
+   */
+  vector<byte> vBuffer;
+
+  /**
+   * Índice de compresión
+   */
+  int iComp;
+
+
+  /**
+   * Tabla donde se almacena cada una de las entradas de pares codificantes.
+   */
+  map<codw, list<byte> > m_vTablaCod;
+  map<list<byte>, codw> m_vTablaCodInv;
+
+ public:
+  lzw(); 
+  ~lzw();
+
+  void readFile (string sFile);
+
+};
+
+#endif
