7933e2230ee305a6b1c05629c85e6c6ea2ca092c
cyberslas <cyberslas@c9134505-e34d-0410-8303-e1b79613182d>
2008-06-01 19:49:48 +0000

    Añadido fichero de test de descompresión LZW

---
+++ trunk/untest_lzw.cpp
@@ -0,0 +1,11 @@
+#include "lzw.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  lzw tmp(true);
+  string sIn(argv[1]);
+  string sOut(sIn + ".out");
+  tmp.uncompress(sIn, sOut);
+  return 0;
+}
