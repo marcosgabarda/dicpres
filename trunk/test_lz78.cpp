3d0b81b8ce1b9551130bc0bcf78acf9b01b353f6
irodrisa <irodrisa@c9134505-e34d-0410-8303-e1b79613182d>
2008-06-01 18:07:15 +0000

    Añadido el test.cpp para LZ78

---
+++ trunk/test_lz78.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "lz78.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+  lz78 tmp;
+  string sIn(argv[1]);
+  string sOut(sIn+".lz78");
+  tmp.readFile(sIn);
+  tmp.compress(sOut);
+  return 0;
+}
