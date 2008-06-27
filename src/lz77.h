3efe3efe821494f1d98c0d93b8a8cf244a8cb4ec
cyberslas <cyberslas@c9134505-e34d-0410-8303-e1b79613182d>
2008-06-27 18:16:53 +0000

    Implementando LZ77.

--- src/lz77.h
+++ src/lz77.h
@@ -10,33 +10,46 @@
 using namespace std;
 
 /**
- * Clase que implementa la compresi&oacute;n de ficheros por el metodo de dicionario
- * LZ77.
+ * \class LZ77
+ *
+ * Clase que implementa la compresi&oacute;n de ficheros por el metodo de 
+ * diccionario LZ77.
+ *
  * \author Iv&aacute;n Rodr&iacute;guez Sastre 
  * \author Marcos Gabarda Inat
  */
 class lz77 {
+
  private:
 
   /**
+   * \var m_vBuffer
+   *
    * Buffer que contiene todo el fichero leido
    */
   vector<byte> m_vBuffer;
 
   /**
+   * \var m_iIni
+   * \var m_iFin
+   *
    * &Iacute;ndices de la ventana de compresi&oacute;n
    */
   int m_iIni, m_iFin;
 
   /**
+   * \var m_nVentana
+   *
    * Tama&ntilde;o de la ventana
    */
   int m_nVentana;
 
   /**
+   * \var m_vTablaCod
+   *
    * Tabla donde se almacena cada una de las entradas de pares codificantes.
    */
-  vector<cod77> vTablaCod;
+  vector<cod77> m_vTablaCod;
 
   /**
    * Procesa la siguiente tupla cod77 que se genera en funci&oacute;n de la 
@@ -44,13 +57,15 @@ class lz77 {
    */
   cod77 obtenerTupla();
 
+  void readFile (string sFile);
+
  public:
 
   lz77(int nVentana = DEFAULT_WINDOW);
 
   ~lz77();
-
-  void readFile (string sFile);
+  
+  void compress (string sFileIn, string sFileOut);
   
 
 };
